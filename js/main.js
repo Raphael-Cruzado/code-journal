7e36939a9e4df7e8e004dd33dc222bc99030f071
Lance Cruzado <raphaellcruzado@gmail.com>
2022-04-05 07:03:20 +0000

    add image on input. #1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 /* global data */
 /* exported data */
+
+var $imageDisplay = document.querySelector('#display-image');
+
+var $photoURL = document.querySelector('#image-input');
+
+$photoURL.addEventListener('input', function (e) {
+  $imageDisplay.src = $photoURL.value;
+  if ($photoURL.value === '') {
+    $imageDisplay.src = 'images/placeholder-image-square.jpg';
+  }
+});
